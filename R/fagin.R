a80730b97a5c77a03c90205c0d3230318ddca92c
Zebulun Arendsee <arendsee@iastate.edu>
2016-11-06 22:25:13 -0600

    Add stub R code and documentation

---
+++ R/fagin.R
@@ -0,0 +1,25 @@
+#' Load a species GFF into the database
+#' 
+#' @param name species name 
+#' @param gff gene feature format file
+load_gff <- function(name, gff) {
+
+}
+
+#' Load a species GFF into the database
+#' 
+#' @param name species name
+#' @param genome file name
+load_genome <- function(name, genome) {
+
+}
+
+#' Load a synteny map into the database
+#' 
+#' @param qname name of query species
+#' @param tname name of target species
+#' @param synmap synteny map file name
+#' @export
+load_synmap <- function(qname, tname, synmap) {
+
+}

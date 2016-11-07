926d5716f5463426298796e25108afa5a6896e3c
Zebulun Arendsee <arendsee@iastate.edu>
2016-11-06 22:01:47 -0600

    Remove everything and build a fresh R package

--- src/report/R/versions.R
+++
@@ -1,8 +0,0 @@
-get_synder_version <- function(){
-  system2('synder', args=list('-v'), stdout=TRUE)
-}
-
-get_fagin_version <- function(config){
-  # TODO: Make this portable
-  system2('cat', args=list(file.path(config$d_home, 'VERSION')), stdout=TRUE) 
-}

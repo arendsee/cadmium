0cb5adbeb4aa5d8a8b601688c7176780aff29add
Zebulun Arendsee <arendsee@iastate.edu>
2016-12-12 17:58:51 -0600

    Add main.R for collation and magrittr

---
+++ R/main.R
@@ -0,0 +1,11 @@
+#' @include dataprep.R
+#' @include effects.R
+#' @include features.R
+#' @include inputs.R
+#' @include validators.R
+#' @include nodes.R
+#' importFrom magrittr "%>%"
+
+# so R doesn't complain about magrittr symbols
+utils::globalVariables(c("%>%", "."))
+NULL

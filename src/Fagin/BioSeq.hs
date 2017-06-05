7b9f2a425c3bd856d65d30398e7084abc25122df
Zebulun Arendsee <arendsee@iastate.edu>
2017-06-05 03:29:15 -0500

    Add some stub material on BioSeq
    I've added stub fasta parsing code. But before I do more, I realize
    there are some gaps in my handling of GFF3. I need to do GFF3 *the right
    way*.  That is, I need to follow the specification, and there is one:
    
    https://github.com/The-Sequence-Ontology/Specifications/blob/master/gff3.md
    
    So now I will go through specification, line by line, ensuring I conform
    to every aspect of the specification. This is a lot of work. While the
    GFF3 format seems trivial (just tab delimited), it isn't. There are a
    lot of details that snag users.
    
    I would like to use an existing library for GFF3 parsing. I might look
    into the Haskell Bio package.
    
    A quick look suggests there is at least one deviation of their
    implementation from the specification: they use char8 bytestrings,
    whereas the specification allows unicode. This is not unreasonable,
    perhaps the gain in memory efficiency (2-fold) and speed is worth it,
    since GFF3 files should usually be ASCII.
    
    Anyway, I will look into their library, study up on MonadError typeclass
    and be back in a few days.

---
+++ src/Fagin/BioSeq.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Fagin.BioSeq (BioSeq(..)) where
+
+import qualified Data.ByteString.Char8 as B
+
+import Fagin.Error
+
+type RawSeq = B.ByteString
+type SeqId = B.ByteString
+type Desc = B.ByteString
+
+data DNA
+data AA
+data Unknown
+
+data BioSeq a
+  = BioSeq {
+      seqid   :: SeqId
+    , seqdesc :: Maybe Desc
+    , seqstr  :: RawSeq
+  }
+
+-- instance Show (BioSeq a) where
+--   show = show . toFasta 
+-- 
+-- seqslice :: BioSeq a -> Int -> Int -> ThrowsError (BioSeq a)
+-- seqslice i j b
+--   | len == j  = Right $ BioSeq newseqid Nothing newseqstr
+--   | len == 0  = Left $ IndexError (msg ++ ", i > seqlen")
+--   | otherwise = Left $ IndexError (msg ++ ", j > seqlen")
+--   where
+--     newseqstr = B.take j . snd . B.splitAt i . seqstr $ b
+--     newseqid = seqid b -- ++ (B.pack "_[") ++ (B.pack . show) i ++ (B.singleton ',') ++ (B.pack . show) j ++ (B.singleton ']')
+--     len = length s
+--     msg = "cannot extract slice (i=" ++
+--           show i ++ ", j=" ++ show j ++ ")" ++
+--           " from '" ++ seqid b ++ "'"
+-- 
+-- fromFasta :: B.ByteString -> [BioSeq Unknown]
+-- fromFasta = map B.split '\n' . B.split '>'
+-- 
+-- toFasta :: BioSeq a -> B.ByteString
+-- toFasta s = concat [">", id', " ", desc', "\n", seq'] where
+--   id'   = seqid s
+--   desc' = fromMaybe " " (seqdesc s)
+--   seq'  = seqstr s

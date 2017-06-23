96082c2bed217692ebc891d7f84594ff51dbc123
Zebulun Arendsee <arendsee@iastate.edu>
2017-06-23 10:43:31 -0500

    Tactical retreat

---
+++ src/Fagin/GeneModel.hs
@@ -0,0 +1,185 @@
+module Fagin.GeneModel (
+    GeneModel(..)
+  , buildModels
+  , model2gff
+) where
+
+import qualified Data.Map.Strict as MS
+import qualified Data.List.Extra as LE
+
+import Fagin.Prelude
+import Fagin.Gff
+import Fagin.Report
+import Fagin.Interval
+
+data GeneModel = GeneModel {
+      model_chrid  :: !ConstantString   -- ^ chromosome/scaffold
+    , model_parent :: ![ConstantString] -- ^ parent (a 'gene') is given
+    , model_id     :: !ConstantString   -- ^ the ID value (must be unique)
+    , model_cds    :: ![Interval]        -- ^ list of coding intervals
+    , model_exon   :: ![Interval]        -- ^ list of exon intervals
+    , model_strand :: !Strand            -- ^ strand [+-.?]
+  } deriving(Show,Generic,NFData)
+
+model2gff :: GeneModel -> ReportS [GffEntry]
+model2gff GeneModel {
+      model_chrid  = chrid'
+    , model_parent = parent'
+    , model_id     = id'
+    , model_cds    = cdss
+    , model_exon   = (exon0:exons) -- require at least one exon
+    , model_strand = strand'
+  } = pass' $ [mrnaGff] ++ cdsGff ++ exonGff
+  where
+    mrnaGff = GffEntry {
+        gff_seqid    = chrid'
+      , gff_type     = MRna
+      , gff_interval = foldr' (<>) exon0 (cdss ++ exons)
+      , gff_strand   = Just strand'
+      , gff_attr     = mrnaAttr parent'
+    }
+
+    mrnaAttr [] = [AttrID id']  
+    mrnaAttr ps = [AttrID id', AttrParent ps]  
+
+    cdsGff = map (newChild CDS) cdss
+    exonGff = map (newChild Exon) (exon0:exons)
+
+    newChild :: IntervalType -> Interval -> GffEntry
+    newChild t i = GffEntry {
+        gff_seqid    = chrid'
+      , gff_type     = t
+      , gff_interval = i
+      , gff_strand   = Just strand'
+      , gff_attr     = [AttrParent [id']]
+    }
+model2gff _ = fail' "InvalidModel: no exons"
+
+type ParentId = ConstantString
+type EntryId = ConstantString
+
+type IdMap = MS.Map EntryId GffEntry
+
+data Parent = Parent ParentId IntervalType deriving(Show,Eq,Ord,Generic,NFData)
+
+requireParent :: ([Parent], GffEntry) -> ReportS ([Parent], GffEntry)
+requireParent ([], g) = case gff_type g of
+  Exon -> fail' $ "FeatureExpectParent: exon must have a parent\n" ++ bshow g
+  CDS  -> fail' $ "FeatureExpectParent: CDS must have a parent\n" ++ bshow g
+  _    -> pass' ([], g)
+requireParent x = pass' x
+
+extractParent :: IdMap -> GffEntry -> ReportS ([Parent], GffEntry)
+extractParent m g =
+
+  -- _ -> ThrowsError ([Parent], GffEntry)
+  fmap (\x -> (x, g)) .
+
+  -- _ -> ThrowsError [Parent]
+  sequenceR .
+
+  -- _ -> [ThrowsError Parent]
+  map (getParent m) .
+
+  -- [ConstantString] -> [ConstantString]
+  -- I have to add this filter because some programs don't follow the specs.
+  -- They add 'Parent' tags to things that don't have parents, like genes (i.e.
+  -- "Parent=-"). They shouldn't do this, they really should know better. But
+  -- I'm a lowly programer, my tools have to magically handle whatever
+  -- malformed trash gets thrown at it.
+  filter (/= "-") .
+
+  -- _ -> [ConstantString] -- list of Parent ids
+  extractParents .
+
+  -- GffEntry -> Attributes
+  gff_attr $ g
+
+  where
+    extractParents :: [Attribute] -> [ConstantString]
+    extractParents attrs = case find isParent attrs of
+      Just (AttrParent ps) -> ps
+      _ -> []
+
+    isParent :: Attribute -> Bool
+    isParent (AttrParent _) = True
+    isParent             _  = False
+
+    getParent :: IdMap -> ConstantString -> ReportS (Parent)
+    getParent m' p = case MS.lookup p m' of
+      Just pg  -> pass' $ Parent p (gff_type pg)
+      Nothing  -> fail' $ unwords
+        [
+            "FeatureInvalidParent: In 'Parent="
+          , (fromShort p)
+          , "', no matchinf 'Id' found\n"
+          , " - Offending line:\n"
+          , bshow g
+        ]
+
+
+mapEntries :: [GffEntry] -> IdMap
+mapEntries = MS.fromList . concatMap plist where
+  plist :: GffEntry -> [(ConstantString, GffEntry)]
+  plist g = case find isID $ gff_attr g of
+    Just (AttrID p)  -> [(p, g)]
+    _ -> []
+
+  isID :: Attribute -> Bool
+  isID (AttrID _) = True
+  isID _          = False
+
+toModels :: [(Parent, GffEntry)] -> ReportS [GeneModel]
+toModels = sequenceR . map toModel . LE.groupSort where
+
+  -- TODO check that each CDS is subsumed by an exon
+  -- TODO check that no exons overlap
+
+  toModel :: (Parent, [GffEntry]) -> ReportS GeneModel
+  toModel ((Parent pid _), gs)
+    = case group . catMaybes . map gff_strand $ gs of
+      [(s:_)] ->  GeneModel
+              <$> getChrid gs  -- model_chrid
+              <*> pure []      -- model_parent
+              <*> pure pid     -- model_id
+              <*> getCDS' gs   -- model_cds
+              <*> getExon' gs  -- model_exon
+              <*> pure s       -- model_strand
+      [] -> fail' $ "InvalidGeneModel: CDS and exon entries specify no strand\n"
+      _  -> fail' $ "InvalidGeneModel: all elements of a gene model must be on the same strand"
+
+  getChrid :: [GffEntry] -> ReportS ConstantString 
+  getChrid gs = case group . map gff_seqid $ gs of
+    [(s:_)] -> pass' s
+    ss -> fail' $ "InvalidGeneModel: model span multiple scaffolds: " ++
+                  unsplit ',' (map fromShort . mapMaybe headMay $ ss)
+
+  getCDS' :: [GffEntry] -> ReportS [Interval]
+  getCDS' gs = pass' . map gff_interval . filter isCDS $ gs
+
+  isCDS :: GffEntry -> Bool
+  isCDS g = case gff_type g of
+    CDS -> True
+    _   -> False
+
+  isExon :: GffEntry -> Bool
+  isExon g = case gff_type g of
+    Exon -> True
+    _    -> False
+
+  getExon' :: [GffEntry] -> ReportS [Interval]
+  getExon' gs = pass' . map gff_interval . filter isExon $ gs
+
+
+mRnaParent :: ([Parent], GffEntry) -> [(Parent, GffEntry)]
+mRnaParent (ps, g) = map (\p -> (p, g)) $ filter isMRna ps where
+  isMRna :: Parent -> Bool
+  isMRna (Parent _ MRna) = True
+  isMRna _               = False
+
+buildModels :: [GffEntry] -> ReportS [GeneModel]
+buildModels gs
+  = (sequenceR . map (extractParent $ mapEntries gs) $ gs) >>=
+    (sequenceR . map requireParent) >>=
+    return . concatMap mRnaParent  >>=
+    toModels
--- src/Fagin/Gff.hs
+++ src/Fagin/Gff.hs
@@ -1,196 +1,373 @@
+{-|
+
+Perhaps the most error prone step of preparing data for Fagin is gathering
+correct GFF files for each species.
+
+Here are the lexical requirements:
+
+ * has 9 TAB-delimited columns
+ * column 1 matches the names of an entry in a genome fasta file
+ * column 3 must include `mRNA`, `exon` and `CDS` labels
+ * column 4 and 5 must be counting numbers where c5 > c4
+ * column 7 must contain strand info
+    [@+@] plus strand
+    [@-@] minus stand
+    [@?@] stranded but unknown
+    [@.@] unstranded
+ * column 9 must be a ';'-delimited string of '<tag>=<value>' entries
+
+I also impose these semantic requirements:
+
+ * Every exon and CDS entry must have at least one Parent, where the value
+   matchs the value of an mRNA's ID
+ * Every mRNA must have at least one exon
+ * Every mRNA must have at least one CDS
+ * Every CDS is subsumed by an exon in the same mRNA
+
+I am systematically more permissive than required by the specification
+
+ * column 1 can be a string of any non-TAB characters
+ * column 8 (phase) can be missing for CDS (I don't use it)
+ * column 6 (score) can be anything (again, I don't use it)
+ * I allow a single untagged value in column 9, which I assign to ID if ID is missing
+
+There are several pathological cases that a GFF parsers needs to be able to
+deal with. These are outlined in the GFF specification:
+
+ * single exon genes
+ * polycistronic transcripts
+ * genes with inteins
+ * trans-spliced trancripts
+ * programmed frameshift
+ * operons
+ * circular genomes (well, I consider this pathological)
+
+All of these need special consideration. Currently they are not handled.
+
+-}
+
+
+module Fagin.Gff (
+    readGff
+  , IntervalType(..)
+  , GffEntry(..)
+  , Attribute(..)
+  -- exported just for benchmarking
+  , readType''
+  , readInt''
+  , readStrand''
+  , readAttributes''
+  , toGff''
+) where
+
+import Data.ByteString.Char8 (readInteger)
+
 import Fagin.Prelude
-import Fagin.Report
 import Fagin.Interval
+import Fagin.Report
 
-data Interval = Interval Start Stop
-
-newtype ScaffoldID = ScaffoldID ByteString
-newtype GeneID     = GeneID     ByteString
-newtype ModelID    = ModelID    ByteString
-newtype ParentID   = ParentID   ByteString
-
-newtype DerivedFrom = DerivedFrom ByteString
-
-data Genome    = Genome    [Scaffold]
-data Scaffold  = Scaffold  ScaffoldID [Gene]
-data Gene      = Gene      GeneID [GeneModel]
-data GeneModel = GeneModel ModelID Orf Mrna Strand
-data Orf       = Orf       [Interval]
-data Trans     = Mrna      MRnaID [Interval]
-
-data GeneEntry = GeneEntry GeneID
-data MrnaEntry = MrnaEntry ModelID  Interval Strand [ParentID]
-data ExonEntry = ExonEntry          Interval Strand [ParentID]
-data CDSEntry  = CDSEntry           Interval Strand [ParentID] [DerivedFrom]
+import qualified Control.Lens as CL
+import Control.Lens.Operators
 
 
--- G0 - Initial graph
---
---    e_ ---n,mt--> m_ --m,p'--> g_
---                  ^            ^
---                 /            /
---    c_ ---cp,m--'----1,1-----'
---
---    where
---    e_ := GffEntry     n  := number of exons in a transcript
---    m_ := GffMRna      m  := number of transcripts in a model
---    c_ := GffCDS       t  := number of mRNA spliced into a transcript (transplicing)
---    g_ := GffGene      c  := number of CDS in a ORF
---                       p  := number of ORFs in a transcript
---                       p' := number of ORFs in a model
+-- | Holds the types that are currently used by Fagin. I may extend this later.
+-- Since these types are required to be Sequence Ontology terms, I really ought
+-- to just import the whole ontology table and allow all terms. Then, since
+-- this is an ontology, I might as well port the relations between the terms.
+-- This would be a time-consuming task, but may be worthwhile eventually.
+data IntervalType
+  = MRna
+  | CDS
+  | Exon
+  | Gene
+  | Other !ConstantString
+  deriving(Eq,Ord,Show,Generic,NFData)
+
+instance BShow IntervalType where
+  bshow MRna      = "mRNA"
+  bshow CDS       = "CDS"
+  bshow Exon      = "exon"
+  bshow Gene      = "gene"
+  bshow (Other t) = (fromShort t)
+
+-- | Attributes of a GFF entry, exactly according to the specification. The
+-- data constructors nearly follow the tag names, except that they have been
+-- converted to camel case, as per Haskell conventions, for example
+-- "Derives_from" is converted to "DerivesFrom".
+data Attribute
+    -- | The unique ID for this entry. Presence in more than one GFF entry
+    -- implies the entries are members of a single discontinuous feature (their
+    -- types should be the same).
+    = AttrID !ConstantString
+
+    -- | The display name of the feature. Does not have to be unique.
+    | AttrName !ConstantString
+
+    -- | List of aliases for the feature (for example locus and model ids)
+    | AttrAlias ![ConstantString]
+
+    -- | List of parents of this feature. Indicates a part_of relationship.
+    | AttrParent ![ConstantString]
+
+    -- | Not currently used by Fagin
+    | AttrTarget !ConstantString
+    
+    -- | Not currently used by Fagin
+    | AttrGap !ConstantString
+
+    -- | Not currently used by Fagin
+    | AttrDerivesFrom !ConstantString
+
+    -- | Free notes about the entry. These notes do not have to be quoted
+    -- (according to the specification). Thus any special characters, which
+    -- include commas, need to be encoded.
+    | AttrNote ![ConstantString]
+
+    -- | A database cross reference
+    | AttrDbxref ![ConstantString]
+
+    -- | Ontology cross reference
+    | AttrOntologyTerm ![ConstantString]
+
+    -- | Is the sequence circular (e.g. a mitochondrial or bacterial genome)
+    | AttrIsCircular !Bool
+
+    -- | The tags defined above are all the tags with predefined meanings.
+    -- Users are free to use any additional flags they desire. These tags must
+    -- be lowercase, since the spec reserves uppercase tags be for future
+    -- official use.
+    | AttrUserDefined !ConstantString !ConstantString
+
+    -- | According to the spec, all entries in the attribute column should be
+    -- wrapped in tag-value pairs. However, it is common for programs to add
+    -- untagged values, which sometimes function as ID.
+    | AttrUntagged !ConstantString
+    deriving(Eq,Ord,Show,Generic,NFData)
+
+instance BShow Attribute where
+  bshow (AttrID           s     ) = "ID="            ++ fromShort s
+  bshow (AttrName         s     ) = "Name="          ++ fromShort s
+  bshow (AttrAlias        ss    ) = "Alias="         ++ unsplit ',' (map fromShort ss)
+  bshow (AttrParent       ss    ) = "Parent="        ++ unsplit ',' (map fromShort ss)
+  bshow (AttrTarget       s     ) = "Target="        ++ fromShort s
+  bshow (AttrGap          s     ) = "Gap="           ++ fromShort s
+  bshow (AttrDerivesFrom  s     ) = "Derives_from="  ++ fromShort s
+  bshow (AttrNote         ss    ) = "Note="          ++ unsplit ',' (map fromShort ss)
+  bshow (AttrDbxref       ss    ) = "Dbxref="        ++ unsplit ',' (map fromShort ss)
+  bshow (AttrOntologyTerm ss    ) = "Ontology_term=" ++ unsplit ',' (map fromShort ss)
+  bshow (AttrIsCircular   True  ) = "Is_circular=true"
+  bshow (AttrIsCircular   False ) = "Is_circular=false"
+  bshow (AttrUserDefined  t v   ) = (fromShort t) ++ "=" ++ (fromShort v)
+  bshow (AttrUntagged     s     ) = "Untagged=" ++ (fromShort s)
 
 
--- G1 - collapse exons into transcripts (assume no trans-splicing)
---
---            T ---m,p'--> g_
---            ^            ^
---         cp,m           /
---          /            /
---    c_ --'----1,1-----'
-collapseExons ::  [ExonEntry] -> MrnaEntry -> Transcript
+-- | Holds the data from a GFF entry that is relevant to Fagin. Some GFF
+-- columns are skipped. These may be added later, but for now I don't need
+-- them.
+data GffEntry = GffEntry {
 
--- G2 - collapse CDS into ORFs
---
---                  T ---m,p'--> g_
---                  ^            ^
---               1,p            /
---               /             /
---        C ----'-----1,1-----'
-collapseCDS :: [CDSEntry] -> Transcript -> ORF
+    -- | GFF column 1. The name of the genomic scaffold and chromosome to which
+    -- the feature maps
+    gff_seqid :: !ConstantString
 
+    -- | GFF column 3. The type of the feature. This must be a Sequence
+    -- Ontology term or identification id.
+    , gff_type :: !IntervalType
 
--- G3 - infer protein names
---
---        C --n,1--> T ---m,p'--> g_
-inferNames :: ORF -> Transcript ->
+    -- | GFF column 4 and 5. The 1-based start and stop positions of this
+    -- feature.
+    , gff_interval :: !Interval
 
+    {-| GFF column 7. The strand on which the interval resides. This must be
+       one of the following:
+        * '+' - plus sense
+        * '-' - negative sense
+        * '.' - strand is irrelevant
+        * '?' - strand is relevant but unknown
+    -}
+    , gff_strand :: !(Maybe Strand)
 
--- toMRna :: [MRnaEntry] -> [ExonEntry] -> ReportS [MRna]
--- toMRna = undefined
---
--- toGffRow :: [ByteString] -> ReportS GffRow
--- toGffRow = undefined
+    -- | GFF column 9. Feature attributes (see 'Attribute')
+    , gff_attr :: ![Attribute]
+  }
+  deriving(Eq,Ord,Show,Generic,NFData)
 
+-- | Construct a GffEntry from the full data of a parsed GFF line
+gffEntry
+  :: ByteString   -- ^ seqid
+  -> ByteString   -- ^ source
+  -> IntervalType -- ^ type
+  -> Integer      -- ^ start
+  -> Integer      -- ^ stop
+  -> ByteString   -- ^ score
+  -> Maybe Strand -- ^ strand
+  -> ByteString   -- ^ phase
+  -> [Attribute]  -- ^ attributes
+  -> GffEntry
+gffEntry seqid _ ftype a b _ strand _ attr = 
+  GffEntry {
+      gff_seqid    = (toShort seqid)
+    , gff_type     = ftype
+    , gff_interval = Interval a b
+    , gff_strand   = strand
+    , gff_attr     = attr
+  }
 
--- partitionTypes :: [GffRow] -> ([GeneEntry], [MRnaEntry], [ExonEntry], [CDSEntry])
--- partitionTypes = undefined
-
-
--- toMatrix :: ByteString -> ReportS [GffRow]
--- toMatrix =
---   sequence
---   map toGffRow                 . -- to record, catch row number errors
---   map (split '\t')             . -- Break tests by line and TAB.
---   filter (\s -> length s == 0) . -- Remove empty lines
---   filter (isPrefixOf "#")      . -- Remove comments
---   split '\n'                     -- this allows space in fields
-
-
--- readGff :: ByteString -> ReportS [Gene]
--- readGff = map toFeature . toMatrix
-
-
-
-
--- data GffRow = GffRow {
---       gff_seqid  :: ByteString
---     , gff_source :: ByteString
---     , gff_type   :: ByteString
---     , gff_start  :: ByteString
---     , gff_stop   :: ByteString
---     , gff_score  :: ByteString
---     , gff_strand :: ByteString
---     , gff_phase  :: ByteString
---     , gff_attr   :: ByteString
---   }
-
-
-
----- Ultimate Goal
--- Genome = Genome [(Seqid, [Gene])]
--- Gene = Gene [(ModelId, [GeneModel])]
--- GeneModel = GeneModel {
---     getTrs :: [Exon]
---   , getTrl :: [CDS]
---   , getStr :: Strand
--- }
+instance BShow GffEntry where
+  bshow GffEntry { 
+      gff_seqid    = seqid
+    , gff_type     = ftype
+    , gff_interval = Interval a b
+    , gff_strand   = strand
+    , gff_attr     = attr
+  } = unsplit '\t'
+    [
+        (fromShort seqid)
+      , "."
+      , bshow ftype
+      , bshow a
+      , bshow b
+      , "."
+      , maybe "." bshow strand
+      , "."
+      , (unsplit ';' . map bshow $ attr)
+    ]
 
 
---   text -> Genome [(Seqid, [Gene])]
---  ==================================================================
---   text
--- ------------------------------------------------------------------
---   [(seqid,...)]
--- ------------------------------------------------------------------
---   [(seqid, [(type,interval,strand,attr)])]
---   ==> Geneome [(Seqid, f x)]
--- ------------------------------------------------------------------
---   f :: [(type,interval,strand,attr)] -> [Gene]
---   
--- ------------------------------------------------------------------
---
---     [Mrna] [CDS] [Exon] [Gene]
---       where
---         Mrna :: ID [Parent]
+type GParser a = ByteString -> ReportS a
+
+readInt'' :: GParser Integer
+readInt'' s = case {-# SCC "gffEntry_integer" #-} readInteger s of
+  Just (x,"") -> pass' x
+  _ -> fail' $ "GffParse: expected integer, found '" ++ s ++ "'"
+
+readType'' :: GParser IntervalType
+readType'' s = {-# SCC "gffEntry_type" #-} case s of
+  ""                -> fail' "GffParse: exected <type> in column 3, found nothing"
+
+  "gene"            -> pass' Gene
+  "SO:0000704"      -> pass' Gene
+
+  "mRNA"            -> pass' MRna
+  "messenger RNA"   -> pass' MRna -- synonym
+  "SO:0000234"      -> pass' MRna
+  -- - this may or may not be a coding transcript
+  -- - technically, mRNA is_a transcript, and a CDS or exon is only transitively
+  --   a part of ta transcript.
+  "transcript"      -> pass' MRna
+  "SO:0000673"      -> pass' MRna
+
+  "CDS"             -> pass' CDS
+  "coding_sequence" -> pass' CDS -- synonym
+  "coding sequence" -> pass' CDS -- synonym
+  "SO:0000316"      -> pass' CDS
+
+  "exon"            -> pass' Exon
+  "SO:0000147"      -> pass' Exon
+  -- This is slightly more specific then exon, hence the different SO id,
+  -- however, it is not terribly common.
+  "coding_exon"     -> pass' Exon
+  "coding exon"     -> pass' Exon -- synonym
+  "SO:0000195"      -> pass' Exon
+  x                 -> pass' $ Other (toShort x)
+
+readStrand'' :: GParser (Maybe Strand)
+readStrand'' s = {-# SCC "gffEntry_strand" #-} case s of
+  "+" -> pass' $ Just Plus
+  "-" -> pass' $ Just Minus
+  "." -> pass' Nothing -- The distinction between '.' and '?' is a nuance I
+  "?" -> pass' Nothing -- will ignore for now
+  v   -> fail' $ "GffParse: expected strand from set [+-.?], found '" ++ v
+
+readAttributes'' :: GParser [Attribute]
+readAttributes'' =
+    sequenceR
+  . map toAttr
+  . map (split '=')
+  . split ';'
+  where
+
+  toAttr :: [ByteString] -> ReportS Attribute
+
+  toAttr ["ID",       v] = {-# SCC "readAttributes_id" #-}     pass' $!! AttrID  (toShort v)
+  toAttr ["Parent" , vs] = {-# SCC "readAttributes_parent" #-} pass' $!! AttrParent $!! map toShort (split ',' vs)
+  
+  toAttr ["Name",   v] = pass' $!! AttrName    (toShort v)
+  toAttr ["Target", v] = pass' $!! AttrTarget  (toShort v)
+  toAttr ["Gap",    v] = pass' $!! AttrGap     (toShort v)
+  toAttr ["Derives_from" , v] = pass' $!! AttrDerivesFrom  (toShort v)
+  -- multiple value entries
+  toAttr ["Alias"         , vs] = pass' $!! AttrAlias        $!! map toShort (split ',' vs)
+  toAttr ["Note"          , vs] = pass' $!! AttrNote         $!! map toShort (split ',' vs)
+  toAttr ["Dbxref"        , vs] = pass' $!! AttrDbxref       $!! map toShort (split ',' vs)
+  toAttr ["Ontology_term" , vs] = pass' $!! AttrOntologyTerm $!! map toShort (split ',' vs)
+  -- boolean entries
+  toAttr ["Is_circular", "true"  ] = pass' $!! AttrIsCircular True
+  toAttr ["Is_circular", "false" ] = pass' $!! AttrIsCircular False
+  toAttr ["Is_circular", _       ] = fail' "GFFAttribute: Is_circular tag must be either 'true' or 'false'"
+
+  -- other entries
+  -- TODO: Note if the tag is upper case, since these should be
+  --       reserved for future use
+  toAttr [v, t] = pass' $!! AttrUserDefined (toShort v) (toShort t)
+
+  -- untagged entry
+  -- TODO interpret untagged value as an ID if no ID is provided The spec does
+  -- not require this, but I should add it in to handle the shit _certain_
+  -- programs throw at us.
+  toAttr [v] = pass' $!! AttrUntagged (toShort v)
+  toAttr []             = fail' $ "GffAttribute: expected attribute (<tag>=<val>), found ''"
+  toAttr fs             = fail' $ concat 
+    ["GffAttribute: expected attribute (<tag>=<val>), found '", unsplit '=' fs, "'"]
 
 
+  -- TODO: should warn about repeats
+  -- warnIfTagsRepeat :: [(ByteString, ByteString)] -> ReportS [(ByteString, ByteString)]
+  -- warnIfTagsRepeat ps = case mapMaybe headMay . map (drop 1) . group . sort . map fst $ ps of
+  --   [] -> pass' ps
+  --   es -> pass' ps >>= warn' ("GFFAttribute: each tag may appear only once, offending tag(s): [" ++ tags ++ "]") where
+  --     tags = unsplit ',' es
 
 
--- type ID = ConstantString
---
--- data Edge = PartOf Node | DerivedFrom Node | PartOf' ID | DerivedFrom' ID
---
--- data Node = Node Feature [Edge]
---
--- data Feature
---   = Gene GeneId [Attribute]
---   | Mrna Start Stop Strand SeqId [Attribute]
---   | Exon Start Stop Strand
---   | CDS  Start Stop Strand
+type GIFilter = (Integer,[ByteString]) -> Bool
 
--- data GeneMode = GeneModel {
---       model_mrna :: MrnaId
---     , model_cds :: [Interval]
---   }
---
--- data GeneModel = GeneModel {
---       model_seqid  :: !SeqId
---     , model_geneid :: !(Maybe GeneId) -- based either on CDS:grandparent or CDS:Derived_from
---     , model_mrnaid :: !(Maybe MrnaId) -- based on CDS:Parent and Exon:Parent
---     , model_cds    :: ![Interval]
---     , model_exon   :: ![Interval]
---     , model_strand :: !Strand
---   }
---
--- type ChildId  = ConstantString
--- type ParentId = ConstantString
--- type GeneId   = ConstantString
--- type MrnaId   = ConstantString
--- type SeqId    = ConstantString
---
--- type Start = Int
--- type Stop  = Int
---
--- data Strand = Plus | Minus | Unset
---
--- newtype Gene = Gene GeneId [Attribute]
--- newtype Mrna = Mrna Start Stop Strand (Maybe GeneId) SeqId [Attribute]
--- newtype Exon = Exon Start Stop Strand MrnaId
--- newtype CDS  = CDS  Start Stop Strand MrnaId (Maybe DerivedFrom)
---
--- data Transcript = Transcript [Interval] Strand SeqId [Parent] [Attribute]
---
--- data Translant  = Translant [Interval] MrnaID (Maybe DerivedID)
---
--- partitionTypes :: [[ByteString]] -> ([Gene],[MRna],[Exon],[CDS])
--- partitionTypes = undefined
---
--- -- | Some exons will have multiple parents, I unravel this particular demon
--- -- here. This step permanently removes Exon types from consideration, wrapping
--- -- them into transcipts.
--- toTranscript :: [MRna] -> [Exon] -> HashMap MrnaId Transcript
---
--- toTranslant :: [CDS] -> [Translant]
---
--- toHash :: [[ByteString]] -> HashMap ParentId
+comment' :: GIFilter
+comment' (_,(x:_)) = isPrefixOf "#" x
+comment' _ = True
 
+empty' :: GIFilter
+empty' (_,[]) = True
+empty' _      = False
+
+toGff'' :: (BShow a) => (a, [ByteString]) -> ReportS GffEntry
+toGff'' (_, [c1,c2,c3,c4,c5,c6,c7,c8,c9])
+  = gffEntry
+  <$> pure             c1 -- seqid   (used as is)
+  <*> pure             c2 -- source  (not used)
+  <*> readType''       c3 -- type
+  <*> readInt''        c4 -- start
+  <*> readInt''        c5 -- stop
+  <*> pure             c6 -- score   (not used)
+  <*> readStrand''     c7 -- strand
+  <*> pure             c8 -- phase   (not used)
+  <*> readAttributes'' c9 -- attributes
+toGff'' (i,fs) = fail' $ concat ["(GFF line ", bshow i, ") Expected 9 columns, found '", bshow (length fs), "'"]
+
+readGff :: ByteString -> ReportS [GffEntry]
+readGff = 
+  sequenceR .
+  map toGff''             . -- Parse GFF entry and report errors. Die on first
+                            -- failed line. Most errors are highly repetitive
+                            -- in GFFs, so just dying on the first failure
+                            -- avoids extremely long error message. A better
+                            -- solution would be to merge the similar errors.
+
+  filter (not . empty')   . -- Filter out lines that are either empty
+  filter (not . comment') . -- or start with a comment (#) character
+
+  zip [1..]               . -- Add line numbers. This must precede filters
+                            -- so line numbering in error messages is correct
+
+  map ( split '\t')       . -- Break tests by line and TAB. NOTE:
+
+  split '\n'                -- this allows space in fields

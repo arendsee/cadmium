36a32fea4819363b049cba8793bbafa21e7a5035
Zebulun Arendsee <arendsee@iastate.edu>
2017-07-01 14:54:15 -0500

    Add nstring finder

--- R/derive.R
+++ R/derive.R
@@ -1,9 +1,32 @@
 #' Derive N-interval table from a genome
 #'
 #' @param dna DNAStringSet genome
-#' @return GenomicRanges object
+#' @return IRanges object
 #' @export
-derive_nstring <- function(dna) { }
+derive_nstring <- function(dna) {
+
+  # TODO: add tests for this 
+
+  # gregexpr search for a pattern, returning the start and width on each given string
+  lapply(dna, function(s) gregexpr("N+", as.character(s))) %>%
+  {
+    data.frame(
+      names = lapply(., '[[', 1) %>% lapply(length) %>% rep.int(names(.), times=.),
+      start = unlist(.) %>% as.vector,
+      width = lapply(., '[[', 1) %>% lapply(attr, "match.length") %>% unlist %>% as.vector
+    )
+  } %>%
+  # gregexpr stores absence of a match as -1 in both start and width
+  dplyr::filter(.data$start != -1) %>%
+  {
+    IRanges::IRanges(
+      start=.$start,
+      width=.$width,
+      names=.$names
+    )
+  }
+
+} 
 
 #' Derive mono-exonic ORF intervals from a genome
 #'

f1e71aa2cd9a84bdac83a5df1f9f5913684df04f
Zebulun Arendsee <arendsee@iastate.edu>
2017-07-01 22:26:55 -0500

    Parameterize dnaregex with strand

--- R/derive.R
+++ R/derive.R
@@ -1,12 +1,48 @@
 #' Internal function for extracting intervals from pattern matches on DNA
 #'
+#' The \code{strand} parameter can be
+#'
+#' \itemize{
+#'   \item 'p' search plus strand
+#'   \item 'm' search minus strand
+#'   \item 'u' search plus strand but report no strand
+#'   \item 'b' search both strands
+#' }
+#'
+#'
 #' @param x       DNAStringSet
 #' @param pattern character regular expression
+#' @param strand  which strand to search (see details)
 #' @param ...     extra arguments for gregexpr
 #' @return GRanges object
-dnaregex <- function(x, pattern, ...){
+dnaregex <- function(x, pattern, strand=c('b'), ...){
   # TODO: add tests for this 
 
+  if(! strand %in% c('p', 'm', 'u', 'b')){
+    stop("IllegalArgument: strand must be 'p', 'm', 'u', or 'b'")
+  }
+
+  if(strand %in% c('p', 'u', 'b')){
+    f <- .dnaregex_unstranded(x, pattern, ...)
+    if(strand != 'u'){
+      GenomicRanges::strand(f) <- '+'
+    }
+  }
+
+  if(strand %in% c('m', 'b')){
+    r <- .dnaregex_unstranded(Biostrings::reverseComplement(x), pattern, ...)
+    GenomicRanges::strand(r) <- '-'
+  }
+
+  switch(strand,
+    p = f,
+    m = r,
+    u = f,
+    b = c(f,r)
+  )
+  
+}
+.dnaregex_unstranded <- function(x, pattern, ...){
   # gregexpr search for a pattern, returning the start and width on each given string
   lapply(x, function(s) gregexpr(pattern, as.character(s), ...)) %>%
   {
@@ -33,7 +69,7 @@ dnaregex <- function(x, pattern, ...){
 #' @export
 derive_nstring <- function(dna) {
 
-  dnaregex(dna, "N+", ignore.case=TRUE)
+  dnaregex(dna, "N+", strand='u', ignore.case=TRUE)
 
 } 
 
@@ -52,12 +88,8 @@ derive_orfgff <- function(dna) {
 
   orfpat <- "ATG(...)+?(TAA|TGA|TAG)"
 
-  f <- dnaregex(dna, orfpat, perl=TRUE, ignore.case=TRUE)
-  r <- dnaregex(Biostrings::reverseComplement(dna), orfpat, perl=TRUE, ignore.case=TRUE)
-  GenomicRanges::strand(f) <- '+'
-  GenomicRanges::strand(r) <- '-'
+  dnaregex(dna, orfpat, strand='b', perl=TRUE, ignore.case=TRUE)
 
-  c(f,r)
 }
 
 #' Derive ORF protein sequences from genome and ORF ranges

a51eea3ec9c65d0329184fb6154149276bb512a1
Zebulun Arendsee <arendsee@iastate.edu>
2017-10-14 23:57:14 -0500

    Add stub gene pages code

---
+++ R/gene-pages.R
@@ -0,0 +1,19 @@
+makeGenePage <- function(x){
+
+  # gene name
+
+  # basic stats - length, exons, position in focal species
+
+  # sequence
+
+  # alignments against each target species, with scores
+
+  # multiple sequence alignment?
+
+  # contextual alignments, including features from targets
+
+  # synder information
+
+  # synder plot
+
+}
